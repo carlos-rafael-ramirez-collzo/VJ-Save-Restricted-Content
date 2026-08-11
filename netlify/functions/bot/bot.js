7c938dff3f6406ca65f0766ecda3774c7cd53e0e
Carlos Rafael Ramírez Collazo <66447267+carlos-rafael-ramirez-collzo@users.noreply.github.com>
2026-08-11 11:19:05 -0400

    Add Netlify function to run Python bot

---
+++ netlify/functions/bot/bot.js
@@ -0,0 +1,34 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+exports.handler = async (event) => {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn('python3', [
+      path.join(__dirname, 'bot.py'),
+      event.body
+    ]);
+
+    let output = '';
+    pythonProcess.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        resolve({
+          statusCode: 200,
+          body: output || 'OK'
+        });
+      } else {
+        reject({
+          statusCode: 500,
+          body: 'Error en el bot'
+        });
+      }
+    });
+  });
+};
